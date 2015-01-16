1f7d77f2539291171bba2e2cc6031ffaab8952c2
Parth Patel <parth2651@hotmail.com>
2015-01-15 21:59:33 -0800

    Checking in DB archival project

---
+++ Feedaggregator/DBArchival/Common/Common.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBArchival
+{
+    public static class Common
+    {
+        public static string AuditCon = Convert.ToString(ConfigurationManager.ConnectionStrings["AuditDBConnection"]);
+    }
+}
---
+++ Feedaggregator/DBArchival/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SVIMPH.Shared.DataLayer;
+using System.Data;
+
+namespace DBArchival
+{
+    class Program
+    {
+         
+        static void Main(string[] args)
+        {
+            try
+            {
+                SqlHelper.ExecuteNonQuery(Common.AuditCon, CommandType.StoredProcedure, "SP_SyncDB");
+            }
+            catch (Exception ex)
+            {
+                //throw;
+            }
+            
+        }
+    }
+}
